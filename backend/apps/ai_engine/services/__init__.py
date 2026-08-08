1e066b564ac3b437f6e2ad501097755e7031576a
Ranthony78 <robert.thony78@gmail.com>
2026-08-08 21:06:36 +0000

    Clear repository for clean project upload

--- backend/apps/ai_engine/services/__init__.py
+++
@@ -1,9 +0,0 @@
-from .analysis_service import AnalysisService
-from .ai_service import AIService
-from .prompt_service import PromptService
-
-__all__ = [
-    "AIService",
-    "PromptService",
-    "AnalysisService",
-]
